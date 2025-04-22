c245d130ac694a8fdaaacbe2e7e8b14949030502
queensajolga <queenanns.sajolga@gmail.com>
2025-04-22 20:35:47 +0800

    added a dropdodwn

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const theme = ref('light')
+const menuItems = ref([
+  { title: 'RENTEE', route: '/renteedashboard' },
+  { title: 'RENTER', route: '/renterdashboard' },
+])
 
 function onClick() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
@@ -35,7 +42,7 @@ function onClick() {
             <v-container>
               <v-row>
                 <v-col cols="12" md="6" class="mx-auto">
-                  <v-card class="mx-auto elevation-10">
+                  <v-card class="mx-auto elevation-24">
                     <v-row class="d-flex justify-center my-6">
                       <v-col cols="12" md="6" class="d-flex justify-center align-center">
                         <v-img
@@ -67,28 +74,56 @@ function onClick() {
                           variant="outlined"
                           bg-color="yellow-lighten-1 rounded-lg elevation-5"
                         ></v-text-field>
-                        <h4 class="text-center font-weight-medium" style="font-size: 15px">
+                        <h4 class="text-center font-semibold" style="font-size: 15px">
                           Forgot Password?
                         </h4>
-                        <v-btn
-                          size="large"
-                          color="yellow-lighten-2"
-                          class="font-weight-bold mt-4 rounded-pill elevation-10"
-                          type="submit"
-                          block
-                          >LOGIN AS</v-btn
-                        >
-                        <v-btn
-                          size="large"
-                          color="yellow-lighten-2"
-                          class="font-weight-bold mt-4 rounded-pill elevation-10"
-                          type="submit"
-                          block
-                          >Sing in with Google</v-btn
-                        >
+
+                        <v-spacer class="my-10"></v-spacer>
+
+                        <div class="text-center">
+                          <v-menu open-on-click>
+                            <template v-slot:activator="{ isHovering, props }">
+                              <v-btn
+                                color="yellow-lighten-2"
+                                v-bind="props"
+                                :elevation="isHovering ? 24 : 10"
+                                size="large"
+                                class="font-weight-bold mt-4 rounded-pill mx-auto pa-6"
+                                type="submit"
+                                block
+                              >
+                                login as
+                              </v-btn>
+                            </template>
+
+                            <v-list class="text-center" bg-color="yellow-lighten-2 rounded-xl">
+                              <v-list-item
+                                v-for="(item, index) in menuItems"
+                                :key="index"
+                                :value="item"
+                                @click="router.push(item.route)"
+                              >
+                                <v-list-item-title>{{ item.title }}</v-list-item-title>
+                              </v-list-item>
+                            </v-list>
+                          </v-menu>
+                        </div>
+
+                        <v-hover v-slot="{ isHovering, props }">
+                          <v-btn
+                            v-bind="props"
+                            :elevation="isHovering ? 24 : 6"
+                            size="large"
+                            color="yellow-lighten-2"
+                            class="font-weight-bold mt-4 rounded-pill mx-auto pa-6"
+                            type="submit"
+                            block
+                            >Sing in with Google</v-btn
+                          >
+                        </v-hover>
                         <v-divider class="my-5"></v-divider>
 
-                        <h5 class="text-center font-weight-medium" style="font-size: 18px">
+                        <h5 class="text-center font-light" style="font-size: 18px">
                           Don't have an account? REGISTER
                         </h5>
                       </v-form>
